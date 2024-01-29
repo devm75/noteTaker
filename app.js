bde3a446b4f9dadcc2d07eb3ca983489e6a2c0c0
mohan dev <bhardwaj1206409@gmail.com>
2024-01-30 02:05:35 +0530

    folder restructuring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
 const app = express();
-import { connectToDB } from "./db/connect.js";
-import userRouter from "./routes/userRoutes.js";
-import 'dotenv/config';
-import { notFound } from "./middleware/notFound.js";
-import { errorHandlerMiddleware } from "./middleware/errorHandler.js";
+import { connectToDB } from "./src/db/connect.js";
+import userRouter from "./src/routes/userRoutes.js";
+import "dotenv/config";
+import { notFound } from "./src/middleware/notFound.js";
+import { errorHandlerMiddleware } from "./src/middleware/errorHandler.js";
 // middlewares
 
 // if we don't use below line of code then we don't have data in req.body
@@ -13,31 +13,25 @@ app.use(express.json());
 // routes
 
 app.get("/hello", (req, res) => {
-    res.send("Task Manager App");
+  res.send("Task Manager App");
 });
 
-app.use('/api/v1/users', userRouter);
-
+app.use("/api/v1/users", userRouter);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
-
 const start = async () => {
+  // kind of makes sense to spin server(express) only when the db is connected successfully!
 
-    // kind of makes sense to spin server(express) only when the db is connected successfully!
-
-    try {
-        await connectToDB(process.env.MONGO_URI);
-        console.log('DB connected!')
-        app.listen(port, console.log(`Server is listening on port ${port}`));
-
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    await connectToDB(process.env.MONGO_URI);
+    console.log("DB connected!");
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 start();
-
